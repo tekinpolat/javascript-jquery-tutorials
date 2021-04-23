7a06422871aca76a5030e18d278ac9fb5a400042
Tekin POLAT <tekin.polat.dpu@gmail.com>
2021-04-23 13:05:53 +0300

    functions

---
+++ functions/func_7.js
@@ -0,0 +1,24 @@
+function test(){
+    console.log("tekin")
+    console.log("tekin")
+    console.log("tekin")
+
+    return "Merhaba";
+    console.log("POLAT")
+}
+
+function test2(sayi){
+
+    if(sayi > 20){   // 23 > 20 => true
+        return true;
+        //console.log("20 den büyük")
+    }
+    //return false;
+    console.log("Merhaba")
+}
+
+test2(23);
+
+
+
+test2(24);
